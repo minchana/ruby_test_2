ef7f8d272b616cb1046fbf486e94ba7a06dca651
minchana <minchanamincha@gmail.com>
2016-10-13 17:04:48 +0530

    comp

--- 1.rb
+++ 1.rb
@@ -8,28 +8,3 @@
 #     end
 # end
 
-
-class Vehicles
-
-  def initialize(list)
-  list=list.chomp
-  @vechicles = {
-  "two wheelers1" =>"Dio",
-  "two wheelers2" => "Hondo",
-  "two wheelers3" =>"CBZ",
-  "two wheelers4" => "TVS",
-  "four wheelers5"=> "Activa",
-  "four wheelers1" => "Duster",
-  "four wheelers2" => "Swift",
-  "four wheelers3" => "BMW",
-  "four wheelers4" =>"I20",
-  "four wheelers5"=> "nano"
-}
-  puts @vechicles[list]
-
-end
-end
-
-brand = 'Input From the user'
-vech_name = gets
-brand= Vehicles.new(vech_name)
---
+++ test/5.rb
@@ -0,0 +1,10 @@
+
+ a=['red', 'violet', 'blue']
+ b=['1','2','3']
+
+
+a = Array.new( 3, Array.new(2, 1) ) 
+a[0][0] = 'red' 
+p a
+
+
