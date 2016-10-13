1515cf394419179d5b5a8c650826fe75a6f45b30
minchana <minchanamincha@gmail.com>
2016-10-13 14:14:00 +0530

    half

---
+++ test/1.rb
@@ -0,0 +1,5 @@
+array = ["98", "10", "1", "22", "8", "4", "67", "109"]
+
+array=array.sort
+array=array.sort.reverse
+
---
+++ test/3.rb
@@ -0,0 +1,8 @@
+def silly_check() 
+num = gets.to_i
+if num<= 5
+	puts"number is less then 5"
+else
+	puts "number is greater then or equal to 5"
+end
+end
